795c6935d567d655a457a4b66420feb195a4c2ce
eliezer droid <eliohernandez94@gmail.com>
2020-04-16 16:01:26 -0600

    add the archives

---
+++ precache-manifest.60632ecf3e32d257930908eecbc0d1a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b010d3660d2a280f8e5f175c2c59e735",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "fabce412a0dadc1ed0d5",
+    "url": "/covid19-tracker/static/css/main.47684d4a.chunk.css"
+  },
+  {
+    "revision": "6f8d42317540daece7f2",
+    "url": "/covid19-tracker/static/js/2.aeb60577.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.aeb60577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabce412a0dadc1ed0d5",
+    "url": "/covid19-tracker/static/js/main.2ad0eb60.chunk.js"
+  },
+  {
+    "revision": "3472a32df87a999f1966",
+    "url": "/covid19-tracker/static/js/runtime-main.763ff21b.js"
+  },
+  {
+    "revision": "a861abc9a02a4af01ebe16ecf79fee22",
+    "url": "/covid19-tracker/static/media/covidImage.a861abc9.png"
+  }
+]);
