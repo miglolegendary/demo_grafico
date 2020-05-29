2acfe16b338bc25b5e244d38e3390f651f0351e7
eliezer droid <eliohernandez94@gmail.com>
2020-05-29 12:59:28 -0600

    bugfix

---
+++ precache-manifest.b6944d8e9877c7ae1016d9bdb6a68e5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc786629f5c95b1bc8d79670c725eb1",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "ec5b1117998a8e548fa0",
+    "url": "/covid19-tracker/static/css/main.ad16c9b0.chunk.css"
+  },
+  {
+    "revision": "d446441ab0185fecf955",
+    "url": "/covid19-tracker/static/js/2.0001ad8f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.0001ad8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5b1117998a8e548fa0",
+    "url": "/covid19-tracker/static/js/main.e85f306f.chunk.js"
+  },
+  {
+    "revision": "3472a32df87a999f1966",
+    "url": "/covid19-tracker/static/js/runtime-main.763ff21b.js"
+  },
+  {
+    "revision": "a861abc9a02a4af01ebe16ecf79fee22",
+    "url": "/covid19-tracker/static/media/covidImage.a861abc9.png"
+  }
+]);
