4d3616846e4e8947b59bedfdc8c6f1080e7f19f4
eliezer droid <eliohernandez94@gmail.com>
2020-07-23 15:59:07 -0600

    add demoFA

---
+++ demoFA/precache-manifest.3d072945d993c873e764a88f648c5299.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f713923d1f1ee8959bc84f8ce43b655",
+    "url": "/demoFA/index.html"
+  },
+  {
+    "revision": "9e4929790fb130d74aa6",
+    "url": "/demoFA/static/css/main.26856034.chunk.css"
+  },
+  {
+    "revision": "6708a090a7b487e558b8",
+    "url": "/demoFA/static/js/2.259c7bc5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/demoFA/static/js/2.259c7bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4929790fb130d74aa6",
+    "url": "/demoFA/static/js/main.76b8e70a.chunk.js"
+  },
+  {
+    "revision": "43aeafbb66df1f78d217",
+    "url": "/demoFA/static/js/runtime-main.32affee7.js"
+  },
+  {
+    "revision": "62f6908cfb7be25fef47e94ee8791411",
+    "url": "/demoFA/static/media/c1.62f6908c.jpg"
+  },
+  {
+    "revision": "4f42f050570f6732327be39916733c0f",
+    "url": "/demoFA/static/media/c3.4f42f050.jpg"
+  },
+  {
+    "revision": "d4dda15cc5dddfedbb96c8aeb447afa5",
+    "url": "/demoFA/static/media/c4.d4dda15c.jpg"
+  },
+  {
+    "revision": "e3d31945155cce0228ad61a6d348e3c7",
+    "url": "/demoFA/static/media/computadora.e3d31945.svg"
+  },
+  {
+    "revision": "58726c0cfd84048d4ed1e72d348a880d",
+    "url": "/demoFA/static/media/deberes.58726c0c.svg"
+  },
+  {
+    "revision": "37d615b821ae5933b7016b8d10a898bb",
+    "url": "/demoFA/static/media/graduado.37d615b8.svg"
+  },
+  {
+    "revision": "5d338ef511dbe7f4792a311462682cf2",
+    "url": "/demoFA/static/media/icomoon.5d338ef5.svg"
+  },
+  {
+    "revision": "5f3c5dec61935ef750937fc2ee3b0bda",
+    "url": "/demoFA/static/media/icomoon.5f3c5dec.ttf"
+  },
+  {
+    "revision": "b51616324632583ea2623181c985b9e7",
+    "url": "/demoFA/static/media/icomoon.b5161632.eot"
+  },
+  {
+    "revision": "ce5fd12a993f4777cc440b15c4139767",
+    "url": "/demoFA/static/media/icomoon.ce5fd12a.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoFA/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f0dd51526f581c4d5a3e17dbd6b825b4",
+    "url": "/demoFA/static/media/logofa.f0dd5152.png"
+  },
+  {
+    "revision": "195acd1cfe167d03b93ef34e21c35447",
+    "url": "/demoFA/static/media/paralax.195acd1c.jpg"
+  },
+  {
+    "revision": "079f17cf6d9df3ef2204e42da7e52887",
+    "url": "/demoFA/static/media/usuario.079f17cf.svg"
+  }
+]);
